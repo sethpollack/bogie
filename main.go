8b88ef8ea992e673179461de5fc80fe7224f2815
Seth Pollack <seth@sethpollack.net>
2017-12-07 13:28:06 -0500

    change sops log level

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/sethpollack/bogie/cmd"
+import (
+	"github.com/sethpollack/bogie/cmd"
+	"github.com/sirupsen/logrus"
+	"go.mozilla.org/sops/logging"
+)
+
+func init() {
+	// change sops log level to error
+	for _, log := range logging.Loggers {
+		log.SetLevel(logrus.ErrorLevel)
+	}
+}
 
 func main() {
 	cmd.Execute()
